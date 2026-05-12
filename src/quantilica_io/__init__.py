def569a0879a8f57525767de3ead67bf6e0010e0
Komesu, D.K. <daniel@dkko.me>
2026-05-12 00:09:46 -0300

    refactor(io): standardize package versioning using importlib.metadata

--- src/quantilica_io/__init__.py
+++ src/quantilica_io/__init__.py
@@ -1,3 +1,8 @@
 """Analytical data processing and conversion layer for Quantilica."""
 
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("quantilica-io")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
